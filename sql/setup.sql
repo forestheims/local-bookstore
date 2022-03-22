f77c9e2a444d5911f16aeb629eade6906a43f28d
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-22 12:19:34 -0700

    started books controller, model and sql tables

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS publishers;
+DROP TABLE IF EXISTS books;
 
 CREATE TABLE publishers (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,4 +9,15 @@ CREATE TABLE publishers (
     city TEXT,
     state TEXT,
     country TEXT
+)
+
+CREATE TABLE books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    released INT NOT NULL
+)
+
+CREATE TABLE publishers_books (
+    publisher_id BIGINT REFERENCES publishers(id)
+    books_id BIGINT REFERENCES books(id)
 )
