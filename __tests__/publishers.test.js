8b9412d4d403d4d9adb81ff432f585b716df00c3
Violet Stewart <vkazoo2@gmail.com>
2022-03-22 11:51:51 -0700

    get all for publishers

--- __tests__/publishers.test.js
+++ __tests__/publishers.test.js
@@ -24,4 +24,11 @@ describe('local-bookstore routes', () => {
     const res = await request(app).post('/api/v1/publishers').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
   });
+
+  // GET ALL
+  it('gets all publishers from table', async () => {
+    await request(app).post('/api/v1/publishers').send(expected);
+    const res = await request(app).get('/api/v1/publishers');
+    expect(res.body).toEqual([{ id: expect.any(String), ...expected }]);
+  });
 });
