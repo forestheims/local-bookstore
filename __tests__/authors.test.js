3cbd7e0360575036b4f5d089f2957c1102710226
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-22 15:14:08 -0700

    getAll authors complete

--- __tests__/authors.test.js
+++ __tests__/authors.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Author = require('../lib/models/Author');
 
 describe('local-bookstore routes', () => {
   beforeEach(() => {
@@ -18,8 +19,16 @@ describe('local-bookstore routes', () => {
     pob: 'Crescent City, California',
   };
 
+  //CREATE
   it('inserts a row into the authors table', async () => {
     const res = await request(app).post('/api/v1/authors').send(expected);
     expect(res.body).toEqual({ id: expect.any(String), ...expected });
+  });
+
+  //SELECT ALL
+  it('gets all rows from authors table', async () => {
+    const authors = await Author.insert(expected);
+    const res = await request(app).get('/api/v1/authors');
+    expect(res.body).toEqual([{ id: authors.id, ...authors }]);
   });
 });
