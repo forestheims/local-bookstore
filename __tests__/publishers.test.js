f016e88776b92cf7176ca1385c6bb3b8f10fdebc
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-03-22 12:10:07 -0700

    getById for publisher table written

--- __tests__/publishers.test.js
+++ __tests__/publishers.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Publisher = require('../lib/models/Publisher');
 
 describe('local-bookstore routes', () => {
   beforeEach(() => {
@@ -29,6 +30,13 @@ describe('local-bookstore routes', () => {
   it('gets all publishers from table', async () => {
     await request(app).post('/api/v1/publishers').send(expected);
     const res = await request(app).get('/api/v1/publishers');
-    expect(res.body).toEqual([{ id: expect.any(String), ...expected }]);
+    expect(res.body).toEqual([{ id: expect.any(String), name: expected.name }]);
+  });
+
+  //GET PUBLISHER BY ID
+  it('gets a publisher by id', async () => {
+    const pub = await Publisher.insert(expected);
+    const res = await request(app).get(`/api/v1/publishers/${pub.id}`);
+    expect(res.body).toEqual({ id: pub.id, ...expected });
   });
 });
